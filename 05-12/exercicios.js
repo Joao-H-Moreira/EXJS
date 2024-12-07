227f017730548073dfbe566b26ef8b0fc71cf88f
João Moreira <110946307+Joao-H-Moreira@users.noreply.github.com>
2024-12-06 21:09:59 -0300

    Update exercicios.js

--- 05-12/exercicios.js
+++ 05-12/exercicios.js
@@ -29,6 +29,8 @@ console.log(carro)
 let pessoa = {
     nome: "Carlos",
     idade: 28
+    cumprimento: function(){
+    return `Olá, meu nome é ${this.nome} e tenho ${this.idade} `
 }
 
-console.log(` olá meu nome é ${pessoa.nome} e tenho ${pessoa.idade} anos`) 
+console.log(pessoa.cumprimento()) 
